497f871410d64ef474f457ff77537f6002280beb
Mozochi <50766817+Mozochi@users.noreply.github.com>
2025-02-22 12:47:29 +0000

    Implemented stop words

--- data_utils.py
+++ data_utils.py
@@ -1,11 +1,16 @@
 ### Text-to-Vector ###
 
+stop_words = {'i', 'a', 'about', 'an', 'are', 'as', 'at', 'be', 'by', 'com', 'for', 'from', 'how', 'in', 'is', 'it', 'of', 'on', 'or', 'that', 'the', 'this', 'to', 'was', 'what', 'when', 'where', 'who', 'will', 'with', 'the'} # Basic stop words
+
 def build_vocab(texts):
     vocab = set()
 
     for text in texts:
         words = text.lower().split() # Splitting the text into words and converting to lowercase for consistency
-        vocab.update(words)
+
+        # Filtering out stop words
+        filtered_words = [word for word in words if word not in stop_words]
+        vocab.update(filtered_words)
 
     return sorted(list(vocab)) # Sorting for consistency 
 
